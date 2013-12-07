6256a8da23749d714a2075f0466170a63dfb8da9
Philip S Doctor <diomedestydeus@gmail.com>
2013-12-07 13:51:57 -0600

    Improved repl hygiene

--- repl.py
+++ repl.py
@@ -1,15 +1,18 @@
 import ast
 import traceback
 
+repl_scope = {}
 
 while True:
     s = raw_input(">>> ")
     parse_s = None
+    
     try:
         parse_s = ast.parse(s)
     except Exception, error:
         print traceback.format_exc()
         continue
+
     is_exec = False
     for i in ast.walk(parse_s):
         if type(i) == ast.Exec:
@@ -19,11 +22,12 @@ while True:
     
     if is_exec:
     	continue
+
     try:
         try:
-            print eval(s) or ""
+            print eval(s, repl_scope) or ""
         except SyntaxError:
-            exec s
+            exec s in repl_scope
     except Exception, error:
         print traceback.format_exc()
 
